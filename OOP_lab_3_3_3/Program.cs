67c7bdaf92c0d281cb046060c9de639ce05d5d57
Роман Данилевич <dwordchanel@gmail.com>
2020-05-28 23:42:38 +0300

    Add project files.

---
+++ OOP_lab_3_3_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("input.txt");
+
+            string text = file.ReadToEnd();
+
+            string[] words = text.Split(new char[] { '\n', '\r', ' ', ':', ';', '.', ',', '?', '!', '(', ')', '{', '}', '[', ']', '@', '#', '№', '$', '^', '%', '&', '*', '/', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string longestWord = words[0];
+
+            foreach (string word in words)
+            {
+                if (longestWord.Length <= word.Length)
+                {
+                    longestWord = word;
+                }
+            }
+
+            StreamWriter newFile = File.CreateText("output.txt");
+
+            newFile.WriteLine("Найдовше слово: {0}", longestWord);
+
+            newFile.Close();
+        }
+    }
+}
